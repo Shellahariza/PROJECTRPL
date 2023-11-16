3197f5157e97b94ad3511f41e2f926745fb18432
mutiashr <mutiashrjh@gmail.com>
2023-11-16 19:44:24 +0700

    edit by mutia

--- dataprofil.php
+++ dataprofil.php
@@ -1,39 +1,39 @@
 <?php
 session_start();
 
-// Check if the form is submitted
+// Koneksi ke database
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "db_rpl";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+// Memproses formulir saat dikirim
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $full_name = $_POST["full_name"];
-    $birth_date = $_POST["birth_date"];
-    $gender = $_POST["gender"];
-    $phone_number = $_POST["phone_number"];
+    $nama_lengkap = $_POST["nama_lengkap"];
+    $tanggal_lahir = $_POST["tanggal_lahir"];
+    $jenis_kelamin = $_POST["jenis_kelamin"];
+    $nomor_telepon = $_POST["nomor_telepon"];
     $email = $_POST["email"];
-    $weight = $_POST["weight"];
-    $height = $_POST["height"];
+    $berat_badan = $_POST["berat_badan"];
+    $tinggi_badan = $_POST["tinggi_badan"];
 
-    // Database connection
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "db_rpl";
-
-    $conn = new mysqli($host, $username, $password, $database);
-
-    if ($conn->connect_error) {
-        die("Koneksi gagal: " . $conn->connect_error);
-    }
-
-    // Insert data into the database
-    $query = "INSERT INTO tb_datauser (full_name, birth_date, gender, phone_number, email, weight, height) VALUES ('$full_name', '$birth_date', '$gender', '$phone_number', '$email', '$weight', '$height')";
+    // Melakukan query untuk memasukkan data ke dalam tabel
+    $query = "INSERT INTO tb_datauser (nama_lengkap, tanggal_lahir, jenis_kelamin, nomor_telepon, email, berat_badan, tinggi_badan)
+              VALUES ('$nama_lengkap', '$tanggal_lahir', '$jenis_kelamin', '$nomor_telepon', '$email', '$berat_badan', '$tinggi_badan')";
 
     if ($conn->query($query) === TRUE) {
         echo '<script>alert("Data berhasil disimpan"); window.location.href = "index2.php";</script>';
     } else {
-        echo '<script>alert("Error: ' . $conn->error . '");</script>';
+        echo '<script>alert("Terjadi kesalahan, data tidak dapat disimpan."); window.location.href = "profil.html";</script>';
     }
-
-    // Close the database connection
-    $conn->close();
 }
+
+// Menutup koneksi database
+$conn->close();
 ?>
